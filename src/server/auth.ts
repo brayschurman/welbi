2a1b842d9cd909a9f83bf6882664a1ee54500e6f
Bray Schurman <brayschurman@icloud.com>
2024-06-09 22:24:34 -0300

    remove imports

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,13 +5,10 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-import { env } from "~/env";
 import { db } from "~/server/db";
 import { fetchUserByEmailAndPassword } from "./api/trpc";
-import { Session } from "@prisma/client";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
